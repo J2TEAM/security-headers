c62b21b1f5e9f3c4d36423ea1eca51ed4204aad0
J2TeaM <junookyo@gmail.com>
2016-02-17 00:42:02 +0700

    Add security-headers.php

---
+++ security-headers.php
@@ -0,0 +1,5 @@
+<?php
+header('Strict-Transport-Security: max-age=16070400; includeSubDomains');
+header('X-Frame-Options: SAMEORIGIN');
+header('X-XSS-Protection: 1; mode=block');
+header('X-Content-Type-Options: nosniff');
